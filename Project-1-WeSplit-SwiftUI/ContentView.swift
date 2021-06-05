53cbdd0b9cac7ab610a243b0302a25bb1eeba311
Baris <karalarbaris@gmail.com>
2021-06-05 12:42:52 +0200

    Reading text from the user with TextField Creating pickers in a form

--- Project-1-WeSplit-SwiftUI/ContentView.swift
+++ Project-1-WeSplit-SwiftUI/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var noOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Check Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $noOfPeople) {
+                        ForEach(0 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
